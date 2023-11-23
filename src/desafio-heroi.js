b1f41cddd71f31377347e729efee935aea165d1a
jan100br <jansenbruno@hotmail.com>
2023-11-23 00:01:15 -0300

    orientações do desafio

---
+++ src/desafio-heroi.js
@@ -0,0 +1,31 @@
+//Neste repositório, insira todos os links e arquivos necessários para seu projeto, seja um arquivo de banco de dados ou um link para o template no Figma.
+ 
+//Dica: Se o expert forneceu um repositório Github, você pode dar um "fork" no repositório dele para organizar suas alterações e evoluções mantendo uma referência direta ao código-fonte original.
+ 
+//Instruções para entrega
+//1️⃣ Desafio Classificador de nível de Herói
+
+//**O Que deve ser utilizado**
+
+//- Variáveis
+//- Operadores
+//- Laços de repetição
+//- Estruturas de decisões
+
+//## Objetivo
+
+//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+//Se XP for menor do que 1.000 = Ferro
+//Se XP for entre 1.001 e 2.000 = Bronze
+//Se XP for entre 2.001 e 5.000 = Prata
+//Se XP for entre 6.001 e 7.000 = Ouro
+//Se XP for entre 7.001 e 8.000 = Platina
+//Se XP for entre 8.001 e 9.000 = Ascendente
+//Se XP for entre 9.001 e 10.000= Imortal
+//Se XP for maior ou igual a 10.001 = Radiante
+
+//## Saída
+
+//Ao final deve se exibir uma mensagem:
+//"O Herói de nome **{nome}** está no nível de **{nivel}**"
